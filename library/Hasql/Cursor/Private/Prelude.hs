b6d774cde2f09a4a6c5ed158f4b0832f4a337065
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-07-24 13:40:46 +0300

    Correct the "transformers" imports

--- library/Hasql/Cursor/Private/Prelude.hs
+++ library/Hasql/Cursor/Private/Prelude.hs
@@ -13,12 +13,12 @@ import BasePrelude as Exports hiding (assert, left, right, isLeft, isRight, erro
 -------------------------
 import Control.Monad.IO.Class as Exports
 import Control.Monad.Trans.Class as Exports
-import Control.Monad.Trans.Cont as Exports hiding (shift, callCC)
-import Control.Monad.Trans.Except as Exports (ExceptT(ExceptT), Except, except, runExcept, runExceptT, mapExcept, mapExceptT, withExcept, withExceptT)
-import Control.Monad.Trans.Maybe as Exports
-import Control.Monad.Trans.Reader as Exports (Reader, runReader, mapReader, withReader, ReaderT(ReaderT), runReaderT, mapReaderT, withReaderT)
-import Control.Monad.Trans.State.Strict as Exports (State, runState, evalState, execState, mapState, withState, StateT(StateT), runStateT, evalStateT, execStateT, mapStateT, withStateT)
-import Control.Monad.Trans.Writer.Strict as Exports (Writer, runWriter, execWriter, mapWriter, WriterT(..), execWriterT, mapWriterT)
+import Control.Monad.Trans.Cont as Exports hiding (shift)
+import Control.Monad.Trans.Except as Exports hiding (liftCallCC, liftListen, liftPass)
+import Control.Monad.Trans.Maybe as Exports hiding (liftCallCC, liftCatch, liftPass)
+import Control.Monad.Trans.Reader as Exports hiding (liftCallCC)
+import Control.Monad.Trans.State.Strict as Exports hiding (liftCatch, liftListen, liftPass)
+import Control.Monad.Trans.Writer.Strict as Exports hiding (liftCallCC, liftCatch)
 import Data.Functor.Identity as Exports
 
 -- profunctors
